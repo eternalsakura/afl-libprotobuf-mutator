4472f55afc9f1c1b4721c015d740e4a2d28dad67
eternalsakura <eternalsakura13@email.com>
2021-11-28 15:12:19 +0800

    [fix] fix aflpp接口修改 by kiprey and sakura

--- src/mutator.cc
+++ src/mutator.cc
@@ -1,5 +1,5 @@
 #include "mutator.h"
-
+#include <fstream>
 #include <iostream>
 #include <unistd.h>
 
@@ -12,6 +12,8 @@
 #include "gen/out.pb.h"
 
 #include <algorithm>
+#include <random>
+#include <cstdlib>
 
 
 void ProtoToDataHelper(std::stringstream &out, const google::protobuf::Message &msg) {
@@ -19,23 +21,38 @@ void ProtoToDataHelper(std::stringstream &out, const google::protobuf::Message &
   const google::protobuf::Reflection *refl = msg.GetReflection();
 
   const unsigned fields = desc->field_count();
+  // std::cout << msg.DebugString() << std::endl;
   for (unsigned i = 0; i < fields; ++i) {
     const google::protobuf::FieldDescriptor *field = desc->field(i);
 
+    // 对于单个 choice
     if (field->cpp_type() == google::protobuf::FieldDescriptor::CPPTYPE_MESSAGE) {
+      // 如果当前是 choice list
       if (field->is_repeated()) {
         const google::protobuf::RepeatedFieldRef<google::protobuf::Message> &ptr = refl->GetRepeatedFieldRef<google::protobuf::Message>(msg, field);
+        // 将每个 choice 打出来
         for (const auto &child : ptr) {
           ProtoToDataHelper(out, child);
+          out << "\n";
         }
+      // 如果当前是某个子 choice
       } else if (refl->HasField(msg, field)) {
         const google::protobuf::Message &child = refl->GetMessage(msg, field);
         ProtoToDataHelper(out, child);
       }
-    } else if (field->cpp_type() ==
-              google::protobuf::FieldDescriptor::CPPTYPE_ENUM) {
-      out << (char)refl->GetEnumValue(msg, field);
-    } else {
+    } 
+    // 对于单个 field
+    else if (field->cpp_type() == google::protobuf::FieldDescriptor::CPPTYPE_INT32) {
+      out << refl->GetInt32(msg, field);
+      if(i < fields - 1) 
+        out << " ";
+    } 
+    else if (field->cpp_type() == google::protobuf::FieldDescriptor::CPPTYPE_STRING) {
+      out << refl->GetString(msg, field);
+      if(i < fields - 1) 
+        out << " ";
+    } 
+    else {
       abort();
     }
 
@@ -43,10 +60,12 @@ void ProtoToDataHelper(std::stringstream &out, const google::protobuf::Message &
 }
 
 // Apparently the textual generator kinda breaks?
-DEFINE_BINARY_PROTO_FUZZER(const Root &root) {
+DEFINE_BINARY_PROTO_FUZZER(const menuctf::ChoiceList &root) {
   std::stringstream stream;
   ProtoToDataHelper(stream, root);
   std::string data = stream.str();
+  std::replace(data.begin(), data.end(), '\n', '|');
+  puts(("ProtoToDataHelper: " + data).c_str());
   puts("====================================================================================================");
   // std::string dbg = root.DebugString();
   // std::replace(dbg.begin(), dbg.end(), '\n', ' ');
@@ -54,39 +73,83 @@ DEFINE_BINARY_PROTO_FUZZER(const Root &root) {
 
 // AFLPlusPlus interface
 extern "C" {
-  size_t afl_custom_mutator(uint8_t *data, size_t size, uint8_t *mutated_out, size_t max_size, unsigned int seed) {
-    // This will reuse the data buffer...
-    // AFLpp currently performs a memcopy from mutated_out
-    // Unfortunately it's not **mutated_out so we end up doing two memcpy
-    // should be fixed upstream after I finish this
-    assert(size <= max_size);
-    memcpy(mutated_out, data, size);
+  static std::default_random_engine engine_pro;
+  static std::uniform_int_distribution<unsigned int> dis(0, UINT32_MAX);
+
+  void *afl_custom_init(void *afl, unsigned int seed) {
+    #pragma unused (afl)
+    engine_pro.seed(seed);
+    // 不直接 return nullptr 是为了绕过 AFL++ 的 deinit 空指针检测
+    return (void*)-1;
+  }
+  
+  void afl_custom_deinit(void *data) {
+    assert(data == (void*)-1);
+  }
+  
+  // afl_custom_fuzz
+  size_t afl_custom_fuzz(void *data, unsigned char *buf, size_t buf_size, unsigned char **out_buf, 
+                         unsigned char *add_buf, size_t add_buf_size, size_t max_size) {
+    #pragma unused (data)
+    #pragma unused (add_buf)
+    #pragma unused (add_buf_size)
+    
+    static uint8_t *saved_buf = nullptr;
+
+    assert(buf_size <= max_size);
+    
+    uint8_t *new_buf = (uint8_t *) realloc((void *)saved_buf, max_size);
+    if (!new_buf) {
+      *out_buf = buf;
+      return buf_size;
+    }
+    saved_buf = new_buf;
+
+    memcpy(new_buf, buf, buf_size);
 
     size_t new_size = LLVMFuzzerCustomMutator(
-      mutated_out,
-      size,
+      new_buf,
+      buf_size,
       max_size,
-      seed
+      dis(engine_pro)
     );
+    *out_buf = new_buf;
     return new_size;
   }
 
-  size_t pre_save_handler(uint8_t *data, size_t size, uint8_t **new_data) {
+  size_t afl_custom_post_process(void* data, uint8_t *buf, size_t buf_size, uint8_t **out_buf) {
+    #pragma unused (data)
     // new_data is never free'd by pre_save_handler
     // I prefer a slow but clearer implementation for now
+    
     static uint8_t *saved_buf = NULL;
 
-    Root msg;
+    menuctf::ChoiceList msg;
     std::stringstream stream;
-    ProtoToDataHelper(stream, msg);
+    // 如果加载成功
+    if (protobuf_mutator::libfuzzer::LoadProtoInput(true, buf, buf_size, &msg)) {
+      ProtoToDataHelper(stream, msg);
+    }
+    else {
+      // printf("[afl_custom_post_process] LoadProtoInput Error\n");   
+      // std::ofstream err_bin("err.bin");
+      // err_bin.write((char*)buf, buf_size);
+
+      abort();
+
+      // 如果加载失败，则返回 Exit Choice
+      /// NOTE: 错误的变异 + 错误的 trim 将会导致 post process 加载失败，尤其是 trim 逻辑。
+      /// TODO: 由于默认的 trim 会破坏样例，因此需要手动实现一个 trim，这里实现了一个空 trim，不进行任何操作
+      ProtoToDataHelper(stream, menuctf::ExitChoice());
+    }
     const std::string str = stream.str();
 
     uint8_t *new_buf = (uint8_t *) realloc((void *)saved_buf, str.size());
     if (!new_buf) {
-      *new_data = data;
-      return size;
+      *out_buf = buf;
+      return buf_size;
     }
-    saved_buf = new_buf;
+    *out_buf = saved_buf = new_buf;
 
     memcpy((void *)new_buf, str.c_str(), str.size());
 
